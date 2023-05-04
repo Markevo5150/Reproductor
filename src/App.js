cd851685b837dad411e1a4088583b90d60498ab7
edlinar <92194385+edlinar@users.noreply.github.com>
2023-05-03 18:15:53 -0600

    modified:   package-lock.json 	modified:   package.json 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   src/App.js 	modified:   src/Component/Musicplayer.js 	modified:   src/My stylesheet/Musicplayer.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Musicplayer from './Component/Musicplayer';
 
+
+
 function App() {
   return (
     <div className="App">
       <div>
         <Musicplayer />
       </div>
+      
     </div>
   );
 }
--- src/Component/Musicplayer.js
+++ src/Component/Musicplayer.js
@@ -1,20 +1,31 @@
-import React from "react"; 
-import '../My stylesheet/Musicplayer.css';
+import React from "react";
+import "../My stylesheet/Musicplayer.css";
+import { SlArrowDown } from "react-icons/sl";
+import { AiFillFastBackward } from "react-icons/ai";
+import { AiOutlineFastForward } from "react-icons/ai";
+import { BsFillPlayFill } from "react-icons/bs";
 
-function Musicplayer () {
-    return (
-        <div className="container-musicplayer">
-            <img src= {require("../Album covers/albumcover-Come as you are.jpg")} alt="Album-Cover"/>
-           <p className="track-name">Come as you are</p>
-           <p className="artist-name">Nirvana</p>
-           <div className="audio-controls">
-            <audio controls></audio>
-           </div>
-           <div className="lyrics">
-            Lyrics
-           </div>
-        </div>
-    );
+function Musicplayer() {
+  return (
+    <div className="container-musicplayer">
+      <img
+        src={require("../Album covers/albumcover-Come as you are.jpg")}
+        alt="Album-Cover"
+      />
+
+      <p className="track-name">Come as you are</p>
+      <p className="artist-name">Nirvana</p>
+
+      <div className="audio-controls">
+        <AiFillFastBackward size="2rem" className="buttonlyrics" />
+        <BsFillPlayFill size="2rem" className="buttonlyrics" />
+        <AiOutlineFastForward size="2rem" className="buttonlyrics" />
+      </div>
+
+      <div className="lyrics">Lyrics</div>
+      <SlArrowDown size="2rem" className="buttonlyrics" />
+    </div>
+  );
 }
 
-export default Musicplayer
+export default Musicplayer;
