b30558624f769ef0c3f617c7be1469a23c1d7e2c
Jonatan Flores <jonatandavidfloresperez@gmail.com>
2023-05-03 19:18:30 -0600

    modified:   src/Component/Musicplayer.js

--- src/Component/Musicplayer.js
+++ src/Component/Musicplayer.js
@@ -8,16 +8,14 @@ import { BsFillPlayFill } from "react-icons/bs";
 function Musicplayer() {
   return (
     <div className="container-musicplayer">
-      <img
-        src={require("../Album covers/albumcover-Come as you are.jpg")}
-        alt="Album-Cover"
-      />
-
+      {/*Album cover*/}
+      <img src={require("../Album covers/albumcover-Come as you are.jpg")}alt="Album-Cover"/>
+      {/*Song tittle*/}
       <p className="track-name">Come as you are</p>
       <p className="artist-name">Nirvana</p>
-
+      {/*Progress bar*/}
       <div className="progressbar" role="progressbar"></div>
-
+      {/*Control audio*/}
       <div className="audio-controls">
         <button className="button">
           <AiFillFastBackward size="2rem" />
@@ -29,7 +27,7 @@ function Musicplayer() {
           <AiOutlineFastForward size="2rem" />
         </button>
       </div>
-
+      {/*Lyrics*/}
       <div className="lyrics">Lyrics</div>
       <SlArrowDown size="2rem" className="buttonlyrics" />
     </div>
