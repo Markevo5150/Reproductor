e4d92c8e8e3f1c6cff1fd75593ad968acfcc3f89
Jonatan Flores <jonatandavidfloresperez@gmail.com>
2023-05-14 09:14:46 -0600

    modified:   src/Component/Controls.js 	modified:   src/Component/Musicplayer.js

--- src/Component/Controls.js
+++ src/Component/Controls.js
@@ -23,18 +23,18 @@ const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress }) => {
   //The requestAnimationFrame API//
   const playAnimationRef = useRef();
 
-  const repeat = useCallback(() => {
-    const currentTime = audioRef.current.currentTime;
-    setTimeProgress(currentTime);
-    progressBarRef.current.value = currentTime;
-    progressBarRef.current.style.setProperty(
-      '--range-progress',
-      `${(progressBarRef.current.value / duration) * 100}%`
-    );
-  
-    playAnimationRef.current = requestAnimationFrame(repeat);
-  }, [audioRef, duration, progressBarRef, setTimeProgress]);
-  
+const repeat = useCallback(() => {
+  const currentTime = audioRef.current.currentTime;
+  setTimeProgress(currentTime);
+  progressBarRef.current.value = currentTime;
+  progressBarRef.current.style.setProperty(
+    '--range-progress',
+    `${(progressBarRef.current.value / duration) * 100}%`
+  );
+
+  playAnimationRef.current = requestAnimationFrame(repeat);
+}, [audioRef, duration, progressBarRef, setTimeProgress]);
+
 
 useEffect(() => {
   if (isPlaying) {
